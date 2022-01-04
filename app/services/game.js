8b29ec02dd5658dfa35b205a025de7af4b9fca6b
Jason Barry <jbarry@hey.com>
2022-01-03 21:02:27 -0500

    Move logic into game service

--- app/services/game.js
+++ app/services/game.js
@@ -1,21 +1,85 @@
 import Service from '@ember/service';
 import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 import Player from '../models/player';
 
 export const MIN_PLAYER_COUNT = 2;
 export const MAX_PLAYER_COUNT = 4;
+const WIN_SCORE = 100;
 
 export default class GameService extends Service {
-  @tracked numberOfPlayers = MIN_PLAYER_COUNT;
   @tracked players;
+  @tracked currentPlayerNumber = 1;
+  @tracked currentNumber = 0;
+  @tracked currentTurnScore = 0;
+  @tracked hasWinner = false;
+
+  get currentPlayer() {
+    return this.players.find((player) => player.number === this.currentPlayerNumber);
+  }
 
   constructor() {
     super(...arguments);
 
     this.players = [];
 
-    for (let i = 0; i < MAX_PLAYER_COUNT; i++) {
-      this.players.push(new Player(i + 1, i < this.numberOfPlayers));
+    for (let i = 0; i < MIN_PLAYER_COUNT; i++) {
+      this.players.push(new Player(i + 1, true));
     }
+  }
+
+  resetGame() {
+    this.players.forEach((player) => {
+      player.score = 0;
+    });
+  }
+
+  nextPlayer() {
+    let nextPlayerNumber = this.currentPlayerNumber + 1;
+
+    if (nextPlayerNumber > this.players.length) {
+      this.currentPlayerNumber = 1;
+    } else {
+      this.currentPlayerNumber = nextPlayerNumber;
+    }
+  }
+
+  @action numberEntered(number) {
+    this.currentNumber = number;
+  }
+
+  @action addToScore() {
+    this.currentTurnScore += this.currentNumber;
+    this.currentNumber = 0;
+  }
+
+  @action pass() {
+    this.currentPlayer.score += this.currentTurnScore;
+    this.currentNumber = 0;
+
+    if (this.currentPlayer.score >= WIN_SCORE) {
+      this.hasWinner = true;
+      return;
+    }
+
+    this.currentTurnScore = 0;
+
+    this.nextPlayer();
+  }
+
+  @action bust() {
+    this.currentTurnScore = 0;
+    this.currentNumber = 0;
+
+    this.nextPlayer();
+  }
+
+  @action newGame() {
+    this.currentPlayerNumber = 1;
+    this.currentTurnScore = 0;
+    this.currentNumber = 0;
+    this.hasWinner = false;
+
+    this.resetGame();
   }
 }
