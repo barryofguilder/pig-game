3fcf8df742a171d432637d88e43ead5985835313
Jason Barry <jbarry@hey.com>
2023-08-05 18:30:24 -0400

    Fix the app.css file

--- app/app.js
+++ app/app.js
@@ -2,7 +2,7 @@ import Application from '@ember/application';
 import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from 'pig-game/config/environment';
-import 'pig-game/assets/pig-game.css';
+import './app.css';
 
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
